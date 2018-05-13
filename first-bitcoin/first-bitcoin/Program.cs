62d6722c080d9f92c72d02be35fb5709d0257c7c
apatra <avishek.patra@outlook.com>
2018-05-13 21:11:34 +0530

    First Feature

--- first-bitcoin/first-bitcoin/Program.cs
+++ first-bitcoin/first-bitcoin/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using NBitcoin;
 
 namespace first_bitcoin
 {
@@ -6,7 +7,7 @@ namespace first_bitcoin
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!" + new Key().GetWif(Network.Main));
         }
     }
 }
